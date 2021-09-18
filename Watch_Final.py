50601d705e8cbed00cc4ba44ec488203b6b94243
Shubham Tiwari <65031177+ShubhGurukul@users.noreply.github.com>
2021-09-18 15:01:45 +0530

    Created Final Clock skeleton

---
+++ Watch_Final.py
@@ -0,0 +1,51 @@
+import turtle
+
+turtle.pensize(5)
+turtle.mode('logo')
+turtle.speed(0)
+
+
+# For hand Back
+def jump(distance, store=0):
+    pass
+
+
+# Draw hand
+def hand(length, top):
+    pass
+
+# make hand shape for all 
+def make_hand_shape(name, length, top):
+    pass
+
+
+# Draw Clock face
+def draw_clockFace(radius):
+    pass
+
+
+    pass
+
+
+def setup():
+    pass
+
+def weekday(t):
+    pass
+
+
+def date(z):
+    pass
+
+
+
+
+def tick():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    turtle.mainloop()
